691219fe78d4a292bf2d86593bc3a5aa94f72bb9
pratamaTS <ekskulkom.ardhi@gmail.com>
2021-03-17 14:11:27 +0700

    Fixing auth checking 17-3-21

--- app/src/main/java/com/example/wyromed/Activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/wyromed/Activity/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.wyromed.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wyromed.Api.SessionManager
 import com.example.wyromed.R
 import java.util.Timer
 import kotlin.concurrent.schedule
@@ -10,13 +11,28 @@ import kotlin.concurrent.schedule
 
 
 class SplashScreenActivity : AppCompatActivity() {
+    var token: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val sessionManager = SessionManager(this)
+
+        sessionManager.fetchAuthToken()?.let {
+            token = it
+        }
+
         Timer("SplashScreen", false).schedule(5000) {
-            val onBoarding = Intent(this@SplashScreenActivity, OnBoardingActivity::class.java)
-            startActivity(onBoarding)
-            finish()
+            if(token != null) {
+                val mainActivity = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                startActivity(mainActivity)
+                finish()
+            }else{
+                val onBoarding = Intent(this@SplashScreenActivity, OnBoardingActivity::class.java)
+                startActivity(onBoarding)
+                finish()
+            }
         }
     }
 
